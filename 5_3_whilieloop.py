6840ea2b71aa830cac6d232d495916d6c4b22922
03230293 <03230293.gcbs@rub.edu.bt>
2024-03-11 12:43:02 +0600

    changes

--- 5_3_whilieloop.py
+++ 5_3_whilieloop.py
@@ -2,7 +2,6 @@ i=0
 while i < 6: #exact condition should be made
     print(i)
     i+=1
-    #print(i) shows the number where it stops
+    print(i) #shows the number where it stops
     
 #can use break, continue and else statement
-
