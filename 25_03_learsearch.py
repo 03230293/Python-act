ba9a50a315a4cb15386d1a09e5e080742d7155b4
03230293 <03230293.gcbs@rub.edu.bt>
2024-03-25 11:51:28 +0600

    Linear search

--- 25_03_learsearch.py
+++ 25_03_learsearch.py
@@ -1,8 +1,9 @@
-def linear_search(arr, target):
+def linear_search(arr, target): #parametern- ar and target
     for i in range(len(arr)):
         if arr[i] == target:
             return i
     return -1
+
 arr = [10, 15, 20, 5, 30] 
 target = 30
 
