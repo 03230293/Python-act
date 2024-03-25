47fe72403d2d902e8ba01cea26a982a60b2807fd
03230293 <03230293.gcbs@rub.edu.bt>
2024-03-25 12:06:37 +0600

    Binary search

--- 25_3_binarysearch.py
+++ 25_3_binarysearch.py
@@ -3,20 +3,24 @@ def birnary_search(arr, x):
     right=len(arr) -1
 
     while left <= right:
-        mid =(left+right)//2
+        mid =(left+right)//2 #takes a whole no.
+        
         if arr[mid]==x:
             return mid
+        
         if arr[mid]<x:
             left=mid+1
 
         if arr[mid]>x:
             right=mid-1
+
     return -1
 
 arr=[2,3,4,6,8,10,12]
 x=10
 
 result=birnary_search(arr, x) 
+
 if result!=-1:
     print("Element fount at index", result)
 else:
