eaacfb564342e5d4700ba4762fbd3315e352d2ad
03230293 <03230293.gcbs@rub.edu.bt>
2024-03-25 12:42:52 +0600

    quick sort

--- 25_3_bubblesort.py
+++ 25_3_bubblesort.py
@@ -4,7 +4,7 @@ def bubble_sort(arr):
     for i in range(n):
         for j in range(0, n-i-1): #starts from 0
             if arr[j]>arr[j+1]:
-                arr[j], arr [j+1]= arr[j+1], arr[j]
+                arr[j], arr [j+1]= arr[j+1], arr[j] #sorts
 
 arr=[66,34,25,12,22,11,90]
 
@@ -13,3 +13,5 @@ bubble_sort(arr)
 print("Sorted array:")
 for i in range(len(arr)):
     print(arr[i])
+
+    #n*n and O(1)
