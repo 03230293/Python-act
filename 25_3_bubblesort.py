21b84491aebea44b65e2172c99c9be09e570215f
03230293 <03230293.gcbs@rub.edu.bt>
2024-03-25 12:23:05 +0600

    Bubble sort

---
+++ 25_3_bubblesort.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    n=len(arr) #7
+
+    for i in range(n):
+        for j in range(0, n-i-1): #starts from 0
+            if arr[j]>arr[j+1]:
+                arr[j], arr [j+1]= arr[j+1], arr[j]
+
+arr=[66,34,25,12,22,11,90]
+
+bubble_sort(arr)
+
+print("Sorted array:")
+for i in range(len(arr)):
+    print(arr[i])
