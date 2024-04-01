df2fbf38569dfa568b5a9b654f064d1aa17b9011
03230293 <03230293.gcbs@rub.edu.bt>
2024-04-01 11:40:56 +0600

    TRy

---
+++ 1_4.try.py
@@ -0,0 +1,15 @@
+n=1
+m=3  
+def add(n, m):
+    if (n<m):
+        for i in range (m):
+                print("hello world")
+
+# add(n, m)                
+print(add(n, m)  )
+                 
+#have to know the syntaxs
+#if functions - if f(x) is called
+#control structure
+#calculate output base on controla structure
+#inputs
