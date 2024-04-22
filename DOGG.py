66feaaf316bfc9a64ef906fe19c4c1cbb9fba98a
03230293 <03230293.gcbs@rub.edu.bt>
2024-04-22 11:08:47 +0600

    TRy cal

--- DOGG.py
+++ DOGG.py
@@ -10,10 +10,12 @@ class Dog:
     def eat(self):
         print("Nom nom nom!")
 
-my_dog = Dog(breed="Labrador Retriever", age=3, 
+my_dog = Dog(breed="Labrador Retriever", age=10, 
 color="Golden")
 # Accessing attributes and calling methods
 print("My dog is a", my_dog.color, my_dog.breed,"and is", my_dog.age , "years old.")
 # my_dog.bark()
 # my_dog.sleep()
 # my_dog.eat()
+cal=my_dog.age//2
+print(cal)
